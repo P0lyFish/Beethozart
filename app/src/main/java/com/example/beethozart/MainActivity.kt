9ddc8a99a21beb4755fddd3fbaec836c8b958375
P0lyFish <tranthephong32@gmail.com>
2021-03-18 00:49:55 +0700

    fix storage permission

--- app/src/main/java/com/example/beethozart/MainActivity.kt
+++ app/src/main/java/com/example/beethozart/MainActivity.kt
@@ -1,30 +1,26 @@
 package com.example.beethozart
 
-import android.app.NotificationChannel
-import android.app.NotificationManager
 import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
 import android.content.ServiceConnection
-import android.net.Uri
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.IBinder
-import android.provider.MediaStore
-import android.view.View
+import android.widget.Toast
 import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import com.example.beethozart.databinding.ActivityMainBinding
-import com.example.beethozart.entities.Song
-import com.example.beethozart.databases.SongDatabase
-import com.example.beethozart.viewmodels.PlayerViewModel
 import com.example.beethozart.services.MusicPlayerService
+import com.example.beethozart.viewmodels.PlayerViewModel
+import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.TedPermission
 import kotlinx.coroutines.*
-import timber.log.Timber
+
 
 const val CHANNEL_ID = "channel"
 
@@ -34,7 +30,7 @@ class MainActivity : AppCompatActivity() {
 
     private var mainActivityJob = Job()
 
-    private val uiScope = CoroutineScope(Dispatchers.Main +  mainActivityJob)
+    private val uiScope = CoroutineScope(Dispatchers.Main + mainActivityJob)
 
     private lateinit var musicPlayerServiceBinder: MusicPlayerService.MusicPlayerServiceBinder
 
@@ -74,6 +70,33 @@ class MainActivity : AppCompatActivity() {
         val navController = this.findNavController(R.id.navHostFragment)
 
         NavigationUI.setupWithNavController(bottomNavigationView, navController)
+
+        //call back after permission granted
+        //call back after permission granted
+        val permissionlistener: PermissionListener = object : PermissionListener {
+            override fun onPermissionGranted() {
+                Toast.makeText(this@MainActivity, "Permission Granted", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
+                Toast.makeText(
+                    this@MainActivity,
+                    "Permission Denied\n$deniedPermissions",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+
+        //check all needed permissions together
+
+        //check all needed permissions together
+        TedPermission.with(this)
+            .setPermissionListener(permissionlistener)
+            .setDeniedMessage("If you reject permission,you can not use this service\n\nPlease turn on permissions at [Setting] > [Permission]")
+            .setPermissions(
+                android.Manifest.permission.READ_EXTERNAL_STORAGE,
+            )
+            .check()
 
         uiScope.launch {
             SongDatabaseBuilder(application).build()
